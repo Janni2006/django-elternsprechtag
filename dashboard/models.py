6086cf367338cc801292e2c9e1eb984f88b941c2
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2022-08-15 07:27:19 +0000

    add event model

--- dashboard/models.py
+++ dashboard/models.py
@@ -29,8 +29,8 @@ class TeacherStudentInquiry(models.Model):
         CustomUser, on_delete=models.CASCADE, limit_choices_to={'role': 1})
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     parent = models.ForeignKey(
-        CustomUser, on_delete=models.SET_NULL, limit_choices_to={'role': 0}, default=None, null=True, blank=True, related_name='%(class)s_parent')  # limit_choices_to={'role': 0} besagt, dass nur Nutzer, wo der Wert role glwich 0 ist eingesetzt werden können, also es wird verhindert, dass Lehrer oder andere als Eltern in Terminen gespeichert werden
+        CustomUser, on_delete=models.CASCADE, limit_choices_to={'role': 0}, default=None, null=True, blank=True, related_name='%(class)s_parent')  # limit_choices_to={'role': 0} besagt, dass nur Nutzer, wo der Wert role glwich 0 ist eingesetzt werden können, also es wird verhindert, dass Lehrer oder andere als Eltern in Terminen gespeichert werden
     reason = models.TextField()
 
-    possible_events = models.ManyToManyField(
-        Event, blank=False, limit_choices_to={'occupied': False})
+    event = models.ForeignKey(
+        Event, on_delete=models.SET_NULL, blank=True, null=True, default=None)
