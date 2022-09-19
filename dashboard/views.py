7d42d00436ac38fdd7ed03bac3bdd950314d1b19
baastian <mattis.vanderbeek@protonmail.com>
2022-09-19 10:00:20 +0200

    funktioniert jzt bei mir aufm schullapi

--- dashboard/views.py
+++ dashboard/views.py
@@ -90,15 +90,14 @@ def bookEventTeacherList(request, teacher_id):
             events.append({'event': event, 'url': reverse(
                 'book_event_per_id', args=[event.id])})
         booked_events = []
-        for event in Event.objects.filter(Q(
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("authentication", "0001_initial"),
-    ]
+        for event in Event.objects.filter(Q(occupied=True), Q(parent=request.user)):
+            booked_events.append({'event': event, 'url': reverse(
+                'event_per_id', args=[event.id])})
+    
+    return render(request, 'dashboard/events/teacher.html', {'teacher': teacher, 'events': events, 'booked_events': booked_events})
 
-    'operations = [
-        migrations.CreateModel(
-            name="Event",'
+@login_required
+@parent_required
 @lead_started
 def bookEvent(request, event_id):  # hier werden final die Termine dann gebucht
     try:
